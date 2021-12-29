0467d3549b7660f0280bf1b0a0bf3a03058aaf3f
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-12-29 14:02:37 -0500

    ADD: estado de reservaciones por defecto

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,11 +8,10 @@ INSERT INTO roles (id_rol, date_created, date_updated, description) VALUES(5, '2
 
 INSERT INTO users (id, active, address, date_created, date_updated, email, first_name, id_rol, last_name, `_password`, phone, username) VALUES(1, 1, 'direccion de prueba', '2021-10-09 10:00:00', '2021-10-09 10:00:00', 'maximopeoficiales@gmail.com', 'Maximo Junior', 4, 'Apaza', '$2a$10$TXWlGGGuuyYEYQO.84403eGcYvWig/sbnkKXJT6O2XpmSyP7pkcF2', '928644700', 'maximopeoficiales');
 
-INSERT INTO `state_reservations` (`id_state_reservation`, `date_created`, `date_updated`, `description`) VALUES
-                                                                                                             (1, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'PENDIENTE'),
-                                                                                                             (12, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'ACEPTADO/EN CAMINO'),
-                                                                                                             (13, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'ACEPTADO/EN ESPERA'),
-                                                                                                             (14, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'EN VIAJE'),
-                                                                                                             (15, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'VIAJE REALIZADO'),
-                                                                                                             (16, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'CANCELADO');
+INSERT INTO `state_reservations` (`id_state_reservation`, `date_created`, `date_updated`, `description`) VALUES (1, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'PENDIENTE');
+INSERT INTO `state_reservations` (`id_state_reservation`, `date_created`, `date_updated`, `description`) VALUES (12, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'ACEPTADO/EN CAMINO');
+INSERT INTO `state_reservations` (`id_state_reservation`, `date_created`, `date_updated`, `description`) VALUES (13, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'ACEPTADO/EN ESPERA');
+INSERT INTO `state_reservations` (`id_state_reservation`, `date_created`, `date_updated`, `description`) VALUES (14, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'EN VIAJE');
+INSERT INTO `state_reservations` (`id_state_reservation`, `date_created`, `date_updated`, `description`) VALUES (15, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'VIAJE REALIZADO');
+INSERT INTO `state_reservations` (`id_state_reservation`, `date_created`, `date_updated`, `description`) VALUES (16, '2021-12-17 00:00:00.000000', '2021-12-17 00:00:00.000000', 'CANCELADO');
 
