a9813f8d24a54742c81644b79c3d1c6bfcae6f5f
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-01-07 00:12:27 -0500

    ADD: correcion en filtro de pagos

--- src/main/java/com/idat/remiseriamvc/demo/repositorys/crud/IPaymentCR.java
+++ src/main/java/com/idat/remiseriamvc/demo/repositorys/crud/IPaymentCR.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface IPaymentCR extends CrudRepository<Payment, Integer> {
-    @Query(value = "SELECT * FROM `payments` r WHERE (r.dateUpdated BETWEEN ?1 AND ?2)", nativeQuery = true)
+    @Query(value = "SELECT * FROM `payments` r WHERE (r.date_updated BETWEEN ?1 AND ?2)", nativeQuery = true)
     List<Payment> getPaymentBetweenDate(String dateInit, String dateEnd);
 }
